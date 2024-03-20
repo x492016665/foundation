6e2ae1f411f43af68da41b07692e9323332cee08
x492016665 <492016665@qq.com>
2024-03-20 13:30:18 +0800

    PrivilegePoint基础数据mapper

---
+++ src/main/java/com/pf/dao/mapper/PrivilegePointMapper.java
@@ -0,0 +1,13 @@
+package com.pf.dao.mapper;
+
+import com.pf.dao.entity.PrivilegePoint;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 系统权限 Mapper 接口
+ *
+ * @author yk_xing
+ */
+public interface PrivilegePointMapper extends BaseMapper<PrivilegePoint> {
+
+}
