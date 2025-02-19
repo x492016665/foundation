6095701747f5f09145277fcff8306f58b03e10ab
x492016665 <492016665@qq.com>
2025-02-19 19:25:57 +0800

    WxUserMapper区域省份mapper

---
+++ src/main/java/com/pf/dao/mapper/ProvinceMapper.java
@@ -0,0 +1,13 @@
+package com.pf.dao.mapper;
+
+import com.pf.dao.entity.Province;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 省份表 Mapper 接口
+ *
+ * @author yk_xing
+ */
+public interface ProvinceMapper extends BaseMapper<Province> {
+
+}
